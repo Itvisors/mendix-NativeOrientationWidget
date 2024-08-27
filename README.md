b814affe49b9109b5bf5a57de8c3a9f6bc5522f3
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2024-08-27 07:29:32 +0200

    Added orientation attribute and onchange action

--- README.md
+++ README.md
@@ -9,9 +9,20 @@ For example, in portrait, render your dashboard by placing four buttons in two l
 ## Usage
 Place widget on your page.
 
-Place content in the two content area's, separate for portrait and landscape. Be sure to use snippets to avoid duplicating your page content.
+Two options, one or both may be used:
+- Place content in the two content area's, separate for portrait and landscape. Be sure to use snippets to avoid duplicating your page content.
+- Use the orientation attribute and action to respond to orientation changes
 
-The widget determines portrait or landscape orientation by comparing the width and height available to it. This is the safest crossplatform way. That means the widget could get confused when other large content is on the same page. A custom header with buttons should not be a problem.
+The orientation attribute receives the current orientation, it must be an enumeration.
+
+The action will be called after the widget is shown and with each orientation change
+
+Create an enumeration with these values: (Caption can be different, use a value that suits you)
+
+| Key       | Caption   |
+| --------- | --------- |
+| portrait  | Portrait  |
+| landscape | Landscape |
 
 When using the widget in an input form, place the widget inside the dataview. Use snippets to place the input elements in the widget. Also, make sure to save input while entering. Otherwise, if the user has typed data but not left the input element and rotates the device, the input of that widget could be lost.
 
@@ -19,7 +30,7 @@ You can check the test app in the GitHub repo.
 
 The maximum size class can be used if your content is scrollable, like a scroll container. Without the maximum size option, no content could be shown at all or scrolling may not work properly.
 
-Listviews don't work well inside this widget. Use this widget inside the listview to show listview item content depending on device rotation rather than separate listviews for each orientation.
+Listviews don't work well inside this widget. Use the orientation attribute to conditionally show, hide or style your listview content.
 
 ## Styling
 
@@ -29,4 +40,4 @@ The widget uses the following classes:
 - content,
 - contentMaximumSize
 
-The MaximumSize classes have flex: 1 to make the widget take the available space. The container and content classes by default have no styling but are included to allow override of styling.
+The MaximumSize classes have `flex: 1` to make the widget take the available space. The container and content classes by default have no styling but are included to allow override of styling.
--- src/NativeOrientationWidget.jsx
+++ src/NativeOrientationWidget.jsx
@@ -1,19 +1,67 @@
-import { createElement, useMemo, useState } from "react";
-import { View } from "react-native";
+import { View, useWindowDimensions } from "react-native";
+import { createElement, useEffect, useMemo, useState } from "react";
 import { defaultWidgetStyle } from "./ui/styles";
 import { mergeNativeStyles } from "@mendix/pluggable-widgets-tools";
 
 export function NativeOrientationWidget(props) {
-    const [currentLayout, setCurrentLayout] = useState(undefined);
-    const { contentPortrait, contentLandscape, useMaximumSize } = props;
+    const { height, width } = useWindowDimensions();
+    const [currentOrientation, setCurrentOrientation] = useState(undefined);
+
+    const ORIENTATION_PORTRAIT = "portrait";
+    const ORIENTATION_LANDSCAPE = "landscape";
+
+    const { useMaximumSize } = props;
+
+    const newOrientation = width > height ? ORIENTATION_LANDSCAPE : ORIENTATION_PORTRAIT;
+
     const styles = mergeNativeStyles(defaultWidgetStyle, props.style);
     const contentStyle = useMaximumSize ? styles.contentMaximumSize : styles.content;
+
+    const { currentOrientationAttr, orientationChangeAction } = props;
+    useEffect(() => {
+        setCurrentOrientation(c => {
+            // console.info(
+            //     "NativeOrientationWidget: setCurrentOrientation state updater, current: [" +
+            //         c +
+            //         "], new: [" +
+            //         newOrientation +
+            //         "]"
+            // );
+            if (!c || newOrientation !== c) {
+                if (currentOrientationAttr && currentOrientationAttr.status !== "available") {
+                    // console.info("NativeOrientationWidget: orientation attribute is not yet available");
+                    return undefined;
+                }
+                if (currentOrientationAttr && currentOrientationAttr.status === "available") {
+                    if (!currentOrientationAttr.readonly) {
+                        // console.info(
+                        //     "NativeOrientationWidget: setCurrentOrientation attribute value to [" + newOrientation + "]"
+                        // );
+                        currentOrientationAttr.setValue(newOrientation);
+                    } else {
+                        console.warn("NativeOrientationWidget: orientation attribute is readonly");
+                    }
+                }
+                if (
+                    orientationChangeAction &&
+                    orientationChangeAction.canExecute &&
+                    !orientationChangeAction.isExecuting
+                ) {
+                    // console.info("NativeOrientationWidget: call orientation change action");
+                    orientationChangeAction.execute();
+                }
+            }
+            return newOrientation;
+        });
+    }, [currentOrientationAttr, newOrientation, orientationChangeAction]);
+
+    const { contentPortrait, contentLandscape } = props;
     const renderContent = useMemo(() => {
-        if (!currentLayout) {
-            // console.info("NativeOrientationWidget: no layout data yet");
+        if (!currentOrientation) {
+            // console.info("NativeOrientationWidget: no orientation yet");
             return <View style={contentStyle} testID={`${props.name}$noLayout`}></View>;
         }
-        if (currentLayout.width > currentLayout.height) {
+        if (currentOrientation === ORIENTATION_LANDSCAPE) {
             // console.info("NativeOrientationWidget: render content for landscape");
             return (
                 <View style={contentStyle} testID={`${props.name}$contentLandscape`}>
@@ -28,16 +76,9 @@ export function NativeOrientationWidget(props) {
                 </View>
             );
         }
-    }, [contentLandscape, contentPortrait, contentStyle, currentLayout, props.name]);
+    }, [contentLandscape, contentPortrait, contentStyle, currentOrientation, props.name]);
     return (
-        <View
-            style={useMaximumSize ? styles.containerMaximumSize : styles.container}
-            testID={props.name}
-            onLayout={eventData => {
-                // console.info("NativeOrientationWidget: onLayout: " + JSON.stringify(eventData.nativeEvent.layout));
-                setCurrentLayout(eventData.nativeEvent.layout);
-            }}
-        >
+        <View style={useMaximumSize ? styles.containerMaximumSize : styles.container} testID={props.name}>
             {renderContent}
         </View>
     );
